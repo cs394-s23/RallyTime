c6269ae80a9ae6d6836aef574afb2aba3997307f
Kyle Jung <kyle12jung@gmail.com>
2023-05-08 19:38:55 -0500

    simplest chat feature implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from './Landing'
 import Dashboard from './Dashboard/Dashboard';
+import Fanclub from './Fanclub/Fanclub'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />}/>
-        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/fanclub" element={<Fanclub />}>
+          <Route path="/fanclub/:docid" element={<Fanclub />} />
+        </Route>
       </Routes>
     </Router>
   )
