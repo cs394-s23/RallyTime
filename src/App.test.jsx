18c33f3acdb1348c33b68d871250b546a96e2817
jdaenzer123 <98292719+jdaenzer123@users.noreply.github.com>
2023-05-22 20:30:56 -0500

    Done with first unit tests!

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,30 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import App from './App'
+import LandingPage from './Landing'
+import Dashboard from './Dashboard/Dashboard'
+import { useAuth } from './Firebase'
+import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app'
+import { BrowserRouter } from 'react-router-dom'
+
+jest.mock('./Firebase')
+jest.mock('firebase/firestore')
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyArqA1i--TGXyuvN1yvf5WJTEjx01V7Tkc",
+//     authDomain: "rally-time-ea874.firebaseapp.com",
+//     projectId: "rally-time-ea874",
+//     storageBucket: "rally-time-ea874.appspot.com",
+//     messagingSenderId: "732935229980",
+//     appId: "1:732935229980:web:963fb55172bc9beacd85d9",
+//     measurementId: "G-FQD742J753"
+// };
+// const fbapp = initializeApp(firebaseConfig);
+// const db = getFirestore(fbapp);
+// const docs = getDocs(collection(db, "fanclub"))
+
+
 
 test('renders App', async () => {
     render(<App />)
@@ -8,3 +32,7 @@ test('renders App', async () => {
     //await screen.findByText('RallyTime');
     expect(await screen.findByText('Welcome to RallyTime')).toBeVisible()
 })
+
+
+
+
