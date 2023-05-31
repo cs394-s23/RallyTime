9ea2925f7ab34d49896ac9fba00d83514fb44b12
ahnjudy <judyahn2023@u.northwestern.edu>
2023-05-30 21:32:51 -0500

    finished create club test with jest

--- src/CreateClub.test.jsx
+++ src/CreateClub.test.jsx
@@ -1,16 +1,10 @@
 import { render, screen, fireEvent } from '@testing-library/react'
-import userEvent from '@testing-library/user-event';
 import React from 'react'
-import App from './App'
-import LandingPage from './Landing'
-import Dashboard from './Dashboard/Dashboard'
-import { useAuth } from './Firebase'
-import { collection, getDocs, getFirestore } from 'firebase/firestore'
-import { initializeApp } from 'firebase/app'
 import { BrowserRouter } from 'react-router-dom'
 import AddClub from './AddClub/AddClub'
-// import { click } from '@testing-library/user-event/dist/click'
-import { click } from '@testing-library/user-event';
+
+jest.mock('./Firebase')
+jest.mock('firebase/firestore')
 
 test('mock creating club', async () => {
     render(
@@ -21,3 +15,14 @@ test('mock creating club', async () => {
    
     expect(await screen.findByText('Athlete', {}, { timeout: 3000})).toBeVisible()
 })
+
+// //TEST THAT PURPOSEFULLY FAILS
+// test('mock creating club failed', async () => {
+//     render(
+//         <BrowserRouter>
+//           <AddClub/>
+//         </BrowserRouter>
+//       );
+   
+//     expect(await screen.findByText('Random', {}, { timeout: 3000})).toBeVisible()
+// })
