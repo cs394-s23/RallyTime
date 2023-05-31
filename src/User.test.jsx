e7138ab1054166b4bfa94ac045e67f66b223b24c
jdaenzer123 <98292719+jdaenzer123@users.noreply.github.com>
2023-05-30 19:27:03 -0500

    Tests

---
+++ src/User.test.jsx
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import App from './App'
+import LandingPage from './Landing'
+import Dashboard from './Dashboard/Dashboard'
+import { useAuth } from './Firebase'
+import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app'
+import { BrowserRouter } from 'react-router-dom'
+
+jest.mock('./Firebase')
+jest.mock('firebase/firestore')
+
+
+//THIS FILE TESTS BOTH MOCK USER AND MOCK DATA
+
+const mockUser = {
+    "displayName": "Jack Daenzer",
+    "uid": "j7QkHjG5FEcxj95fr7CVHfAw8vo2"
+}
+
+// const mockManager = {
+//     "displayName": "Jack Daenzer",
+//     "uid": "j7QkHjG5FEcxj95fr7CVHfAw8vo2"
+// }
+
+const mockFanClubMembers = [mockUser]
+
+const mockFanClubDoc = {
+    data: () => ({"members": mockFanClubMembers, "athlete": "Test Athlete Name", "manager": mockUser})
+}
+
+const mockFanclubDocList = [mockFanClubDoc]
+
+// //THIS ONE IS SUPPOSED TO FAIL, AS IT DOESN'T SIGN IN A USER. UNCOMMENT IT TO SEE!
+// test('no user on dashboard, requires signin', async () => {
+//     getDocs.mockReturnValue(mockFanclubDocList)
+//     // jest.fn().mockReturnValue(mockFanClubData)
+//     render(
+//         <BrowserRouter>
+//           <Dashboard />
+//         </BrowserRouter>
+//       );
+//     expect(await screen.findByText('See More', {}, { timeout: 3000})).toBeVisible()
+// })
+
+test('mock user on dashboard', async () => {
+    useAuth.mockReturnValue(mockUser)
+    getDocs.mockReturnValue(mockFanclubDocList)
+    render(
+        <BrowserRouter>
+          <Dashboard />
+        </BrowserRouter>
+      );
+    expect(await screen.findByText('Enter Club', {}, { timeout: 3000})).toBeVisible()
+})
