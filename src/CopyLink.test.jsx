b61a35798fb0f2f6239fff6dd0762bf381ed8ccb
ahnjudy <judyahn2023@u.northwestern.edu>
2023-06-06 20:21:18 -0500

    both tests work

--- src/CopyLink.test.jsx
+++ src/CopyLink.test.jsx
@@ -1,26 +1,34 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import Fanclub from './Fanclub/Fanclub';
+import { useAuth } from './Firebase';
+import { doc, getDoc } from 'firebase/firestore';
 
-jest.mock('./Firebase')
-jest.mock('firebase/firestore')
+jest.mock('./Firebase');
+jest.mock('firebase/firestore');
 
 const mockUser = {
-    "displayName": "Jack Daenzer",
-    "uid": "j7QkHjG5FEcxj95fr7CVHfAw8vo2"
-}
+  displayName: 'Jack Daenzer',
+  uid: 'j7QkHjG5FEcxj95fr7CVHfAw8vo2',
+};
 
-// const mockManager = {
-//     "displayName": "Jack Daenzer",
-//     "uid": "j7QkHjG5FEcxj95fr7CVHfAw8vo2"
-// }
+const mockManager = {
+  displayName: 'Jack Daenzer',
+  uid: 'j7QkHjG5FEcxj95fr7CVHfAw8vo2',
+};
 
-const mockFanClubMembers = [mockUser]
+const mockDMs = [];
+
+const mockFanClubMembers = [mockUser];
 
 const mockFanClubDoc = {
-    data: () => ({"members": mockFanClubMembers, "athlete": "Test Athlete Name", "manager": mockUser})
-}
-
-const mockFanclubDocList = [mockFanClubDoc]
+  data: () => ({
+    members: mockFanClubMembers,
+    athlete: 'Connor',
+    manager: mockManager,
+    direct_messages: mockDMs
+  }),
+  exists: () => (true)
+};
 
 Object.assign(navigator, {
   clipboard: {
@@ -28,12 +36,13 @@ Object.assign(navigator, {
   },
 });
 
-test('copyInviteLink should put copy link in the clipboard', () => {
-  render(<Fanclub />);
-  useAuth.mockReturnValue(mockUser)
-  getDocs.mockReturnValue(mockFanclubDocList)
+test('copyInviteLink should put copy link in the clipboard', async () => {
+  useAuth.mockReturnValue(mockUser);
+  getDoc.mockReturnValue(mockFanClubDoc);
 
-  // Mock the current URL
+  render(<Fanclub />);
+
+  // Mock  current URL
   delete window.location;
   window.location = {
     href: 'https://rally-time-ea874.firebaseapp.com/fanclub/zQf4SQNa8AjUw9nfUiGg',
@@ -47,6 +56,31 @@ test('copyInviteLink should put copy link in the clipboard', () => {
   );
 
   // Check if the "Copied!" text is displayed
-  const copiedText = screen.getByText('Copied!');
+  const copiedText = await screen.findByText('Copied!');
   expect(copiedText).toBeInTheDocument();
 });
+
+// //THIS TEST IS SUPPOSED TO FAIL
+// test('copyInviteLink should put copy link in the clipboard', async () => {
+//   useAuth.mockReturnValue(mockUser);
+//   getDoc.mockReturnValue(mockFanClubDoc);
+
+//   render(<Fanclub />);
+
+//   // Mock  current URL
+//   delete window.location;
+//   window.location = {
+//     href: 'https://rally-time-ea874.firebaseapp.com/fanclub/abcdefghijk',
+//   };
+
+//   const copyButton = screen.getByText('Copy Invite Link');
+//   fireEvent.click(copyButton);
+
+//   expect(navigator.clipboard.writeText).toHaveBeenCalledWith(
+//     'https://rally-time-ea874.firebaseapp.com/fanclub/zQf4SQNa8AjUw9nfUiGg'
+//   );
+
+//   // Check if the "Copied!" text is displayed
+//   const copiedText = await screen.findByText('Copied!');
+//   expect(copiedText).toBeInTheDocument();
+// });
